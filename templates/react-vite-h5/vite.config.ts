830cf9b7904b00ef757d01ef25fa20476e3bc5ce
hahaha <1802758314@qq.com>
2023-12-22 18:07:18 +0800

    feat: pxtorem

--- templates/react-vite-h5/vite.config.ts
+++ templates/react-vite-h5/vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 import postcsspxtoviewport from 'postcss-px-to-viewport';
+import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteMockServe({
+      mockPath: 'mock',
+      // enable: import.meta.env.MODE === '',
+    }),
+  ],
   css: {
     postcss: {
       plugins: [
@@ -14,8 +21,8 @@ export default defineConfig({
           viewportWidth: 375, // UI设计稿的宽度
           unitPrecision: 6, // 转换后的精度，即小数点位数
           propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
-          viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
-          fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+          viewportUnit: 'rem', // 指定需要转换成的视窗单位，默认vw
+          fontViewportUnit: 'rem', // 指定字体需要转换成的视窗单位，默认vw
           selectorBlackList: ['ignore-'], // 指定不转换为视窗单位的类名，
           minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
           mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
@@ -28,8 +35,11 @@ export default defineConfig({
     },
   },
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
   },
 });
